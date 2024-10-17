5672810e92511fabc51b2b1cb5c08db3cdfbcf3c
Luis Alfaro <52721684+luisguztav20@users.noreply.github.com>
2024-10-17 13:48:47 -0600

    Modicacion de page login

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,36 +1,53 @@
 <template>
-  <div class="row justify-center">
-    <div class="col-10">
-      <h1 class="text-h4 text-weight-regular text-center">
-        Monitoreo volcanico
-      </h1>
-      <p class="text-h5 text-weight-light text-center">Iniciar sesión</p>
-    </div>
-
-    <div class="form col-10 col-sm-6 col-md-5 col-lg-4 q-gutter-y-lg">
-      <q-input outlined v-model="text" label="Correo" :dense="dense" class="" />
-      <q-input
-        outlined
-        type="password"
-        v-model="text"
-        label="Contraseña"
-        :dense="dense"
-      />
-      <div class="column q-gutter-y-lg">
-        <q-btn
-          color="primary"
-          label="Iniciar sesión"
-          @click="onClick"
-          rounded
-        />
-        <q-btn color="btn-secondary" text-color="black">
-          <q-avatar size="24px" class="q-mr-sm">
-            <img src="/public/icons/google.png" />
-          </q-avatar>
-          Inicias sesión con google
-        </q-btn>
-      </div>
-    </div>
+  <div class="row justify-center q-py-md items-center">
+    <q-card
+      class="my-card col-auto col-sm-auto col-md-auto col-lg-auto col-xl-auto content-center q-pa-md"
+    >
+      <q-card-section>
+        <div class="column q-gutter-y-sm">
+          <img
+            src="/public/icons/logo-log.png"
+            alt="Logo monitoreo volcanico"
+          />
+          <h1 class="text-h5 text-weight-regular text-center">
+            Monitoreo volcanico
+          </h1>
+          <p class="text-h6 text-weight-light text-center">Iniciar sesión</p>
+        </div>
+      </q-card-section>
+      <q-card-section>
+        <div class="form q-gutter-y-lg">
+          <q-input
+            outlined
+            v-model="text"
+            label="Correo"
+            :dense="dense"
+            class=""
+          />
+          <q-input
+            outlined
+            type="password"
+            v-model="text"
+            label="Contraseña"
+            :dense="dense"
+          />
+          <div class="column q-gutter-y-md">
+            <q-btn
+              color="primary"
+              label="Iniciar sesión"
+              @click="onClick"
+              rounded
+            />
+            <q-btn outline color="primary" rounded>
+              <q-avatar size="20px" class="q-mr-sm">
+                <img src="/public/icons/google.png" />
+              </q-avatar>
+              Iniciar con google
+            </q-btn>
+          </div>
+        </div>
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
