6fc7e319f4fb23a29e7a3a8b1f6ead92837f9990
Luis Alfaro <52721684+luisguztav20@users.noreply.github.com>
2024-10-18 13:37:00 -0600

    En el componente IndexPage.vue cree la vista de carga de datos segun mockup

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,45 @@
 <template>
   <q-page>
-    <div>
-      <h1>Actividad sismica</h1>
+    <div class="row justify-center q-py-md items-center">
+      <q-card
+        class="my-card col-10 col-sm-6 col-md-5 col-lg-4 col-xl-2 content-center q-pa-md"
+      >
+        <q-card-section>
+          <h2 class="text-h5">Carga de datos</h2>
+          <div class="column q-gutter-y-lg">
+            <q-input
+              v-model="date"
+              type="date"
+              label="Fecha"
+              stack-label
+              :dense="dense"
+            />
+            <q-select
+              v-model="model"
+              :options="optionsSelect"
+              label="Estacion"
+            />
+            <q-input v-model="time" type="time" label="Hora" />
+            <q-select v-model="model" :options="optionsEje" label="Eje" />
+            <q-btn color="primary" label="Guardar datos" rounded />
+            <q-linear-progress
+              dark
+              rounded
+              indeterminate
+              color="green"
+              class="q-mt-xl"
+            />
+          </div>
+        </q-card-section>
+        <q-card-section> </q-card-section>
+      </q-card>
     </div>
   </q-page>
 </template>
 
-<script setup></script>
+<script setup>
+const optionsSelect = ["Estacion A", "Estacion B", "Estacion C"];
+const optionsEje = ["X", "Y", "Z"];
+</script>
 
 <style lang="scss" scoped></style>
